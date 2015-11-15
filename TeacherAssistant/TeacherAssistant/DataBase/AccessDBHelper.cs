72855ff221f5d47169b742a75bc1cca52c7b3c50
Lin Jie <ljlovemichael@hotmail.com>
2015-11-15 19:32:02 +0800

    创建表，插入数据。

--- TeacherAssistant/TeacherAssistant/DataBase/AccessDBHelper.cs
+++ TeacherAssistant/TeacherAssistant/DataBase/AccessDBHelper.cs
@@ -1,6 +1,7 @@
 ﻿using ADOX;
 using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,16 @@ namespace TeacherAssistant.DataBase
 {
     public static class AccessDBHelper
     {
-        //创建数据库文件
+        //数据库对象
         private static ADOX.Catalog catalog = new Catalog();
         const string connectstrbegin = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=";
         const string connectstrend = ";Jet OLEDB:Engine Type=5";
+        private static OleDbConnection connection;
+        //创建数据库文件
         public static void CreateDB(string path)
         {
             try
             {
-
                 string connectstr = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}; Jet OLEDB:Engine Type = 5", path);
                 catalog.Create(connectstr);
                 Debug.WriteLine("DataBase created");
@@ -31,27 +33,54 @@ namespace TeacherAssistant.DataBase
         public static void ConnectDB(string path)
         {
 
-            //建表
-            ADODB.Connection cn = new ADODB.Connection();
-            cn.Open(connectstrbegin + path, null, null, -1);
-            catalog.ActiveConnection = cn;
+            //连接字符串
+            string connstr = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", path);
+            connection = new OleDbConnection(connstr);
+            //connection.Open();
 
-            ADOX.Table table = new ADOX.Table();
-            table.Name = "ClassTable";
+            //ADODB.Connection cn = new ADODB.Connection();
+            ////打开连接
+            //cn.Open(connstr, null, null, -1);
+            //catalog.ActiveConnection = cn;
 
-            ADOX.Column column = new ADOX.Column();
-            column.ParentCatalog = catalog;
-            column.Name = "RecordId";
-            column.Type = DataTypeEnum.adInteger;
-            column.DefinedSize = 9;
-            column.Properties["AutoIncrement"].Value = true;
-            table.Columns.Append(column, DataTypeEnum.adInteger, 9);
-            table.Keys.Append("FirstTablePrimaryKey", KeyTypeEnum.adKeyPrimary, column, null, null);
-            table.Columns.Append("CustomerName", DataTypeEnum.adVarWChar, 50);
-            table.Columns.Append("Age", DataTypeEnum.adInteger, 9);
-            table.Columns.Append("Birthday", DataTypeEnum.adDate, 0);
-            catalog.Tables.Append(table);
-            cn.Close();
+            //ADOX.Table table = new ADOX.Table();
+            //table.Name = "ClassTable";
+            //ADOX.Column column = new ADOX.Column();
+            //column.ParentCatalog = catalog;
+            //column.Name = "RecordId";
+            //column.Type = DataTypeEnum.adInteger;
+            //column.DefinedSize = 9;
+            //column.Properties["AutoIncrement"].Value = true;
+            //table.Columns.Append(column, DataTypeEnum.adInteger, 9);
+            //table.Keys.Append("FirstTablePrimaryKey", KeyTypeEnum.adKeyPrimary, column, null, null);
+            //table.Columns.Append("CustomerName", DataTypeEnum.adVarWChar, 50);
+            //table.Columns.Append("Age", DataTypeEnum.adInteger, 9);
+            //table.Columns.Append("Birthday", DataTypeEnum.adDate, 0);
+            //catalog.Tables.Append(table);
+            //cn.Close();
+        }
+        public static void CreateTable(string sql,string path)
+        {
+            string connstr = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", path);
+            connection = new OleDbConnection(connstr);
+            OleDbCommand cmd = new OleDbCommand(sql, connection);
+            try
+            {
+
+                connection.Open();
+                int affect= cmd.ExecuteNonQuery();
+                Debug.WriteLine("受影响的行数{0}", affect);
+            }
+            catch (Exception exec)
+            {
+
+                Debug.WriteLine(exec.Message);
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
         }
     }
 }
--- TeacherAssistant/TeacherAssistant/MainWindow.xaml.cs
+++ TeacherAssistant/TeacherAssistant/MainWindow.xaml.cs
@@ -40,16 +40,30 @@ namespace TeacherAssistant
             string teachernum = TeacherNumberBox.Text.Trim();
             string filename = String.Format("{0}.mdb", teachernum);
             FileInfo dbfile = new FileInfo(filename);
+            string filePath = new DirectoryInfo(".").FullName + "\\" + filename;
             if (dbfile.Exists)
             {
                 Debug.WriteLine("File exists");
+                //AccessDBHelper.ConnectDB(filePath);
+                List<ClassDetail> classtable = TableHelp.GetClassTable(this.TeacherNumberBox.Text);
+                string itempatten = "insert into classtable (classname,lastweeks, classtype, subject, stuclasses,studentlistrul, classday , classtime) values";
+                foreach (var item in classtable)
+                {
+                    string insert = String.Format("('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')", item.Name, item.LastWeeks, item.ClassType, item.Subject, item.StuClasses, item.StudentListURL, item.Day, item.Time);
+                    string sql = itempatten + insert;
+                    AccessDBHelper.CreateTable(sql,filePath);
+                }
+                //todo 连接数据库，读取数据
             }
             else
             {
+                //创建数据库
+                AccessDBHelper.CreateDB(filePath);
+                //AccessDBHelper.ConnectDB(filePath);
+                AccessDBHelper.CreateTable("create table classtable (id autoincrement primary key, classname TEXT(100), lastweeks TEXT(100), classtype TEXT(100), subject TEXT(100), stuclasses text(100) ,studentlistrul TEXT(100), classday TEXT(100), classtime text(100))",filePath);
+                //todo 存入数据
                 //内网获取课表
                 //List<ClassDetail> classtable = TableHelp.GetClassTable(this.TeacherNumberBox.Text);
-                string filePath = new DirectoryInfo(".").FullName +"\\" +filename;
-                AccessDBHelper.CreateDB(filePath);
             }
             //TableHelp.GetClassTable("041212"); 
             Window clw = new ClassListWindow();
