668812865664dfa7a6c7372413cffb13fcb484ea
Lin Jie <ljlovemichael@hotmail.com>
2015-12-01 01:16:58 +0800

    删掉没用的枚举

--- TeacherAssistant/TeacherAssistant/Model/ClassDetail.cs
+++ TeacherAssistant/TeacherAssistant/Model/ClassDetail.cs
@@ -7,16 +7,6 @@ using System.Threading.Tasks;
 namespace TeacherAssistant.Model
 {
 
-    public enum week : byte
-    {
-        星期一,
-        星期二,
-        星期三,
-        星期四,
-        星期五,
-        星期六,
-        星期日,
-    }
     public class ClassDetail : IComparer<ClassDetail>
     {
         /// <summary>
