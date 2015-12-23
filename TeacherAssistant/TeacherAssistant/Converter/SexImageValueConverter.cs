fd1de1cb60e6e8ca2a9cca6bb74d7a89993bb61a
Lin Jie <ljlovemichael@hotmail.com>
2015-12-24 02:20:01 +0800

    点名模块，加语音

---
+++ TeacherAssistant/TeacherAssistant/Converter/SexImageValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TeacherAssistant.Converter
+{
+    public class SexImageValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string sex = (string)value;
+            if (sex.Contains("男"))
+            {
+                return "/Image/Boy-100.png";
+            }
+            else if(sex.Contains("女"))
+            {
+                return "/Image/Girl-100.png";
+            }
+            else
+            {
+                return "/image/Genderqueer-100.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TeacherAssistant/TeacherAssistant/View/CoursePage.xaml.cs
+++ TeacherAssistant/TeacherAssistant/View/CoursePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TeacherAssistant.DataBase;
 using TeacherAssistant.Model;
 using TeacherAssistant.ViewModel;
+using DotNetSpeech;
 
 namespace TeacherAssistant.View
 {
@@ -22,7 +25,12 @@ namespace TeacherAssistant.View
     /// </summary>
     public partial class CoursePage : Page
     {
+        SpVoice speech = new SpVoice();
+        int speechrate = 0;
+        int volume = 100;
         public CoursePageViewModel vm;
+        List<TeachClassStu> studatalist;
+        int selected = 0;
         public CoursePage()
         {
             InitializeComponent();
@@ -42,5 +50,52 @@ namespace TeacherAssistant.View
             ClassListWindow clw = new ClassListWindow();
             clw.Show();
         }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            studatalist = new List<TeachClassStu>();
+            //AccessDBHelper.ConnectDB(App.Databasefilepath);
+            string sql = "select * from A041518124736";
+            await Task.Run(() =>
+            {
+                OleDbDataReader reader = AccessDBHelper.ExecuteReader(sql, App.Databasefilepath);
+                while (reader.Read())
+                {
+                    TeachClassStu tcsa = new TeachClassStu();
+                    tcsa.StuNum = reader["stunum"].ToString();
+                    tcsa.StuName = reader["stuname"].ToString();
+                    tcsa.Sex = reader["sex"].ToString();
+                    tcsa.Subject = reader["subject"].ToString();
+                    tcsa.ClassNum = reader["classnum"].ToString();
+                    tcsa.ClassState = reader["classstate"].ToString();
+                    tcsa.ClassType = reader["classtype"].ToString();
+                    tcsa.Num = Convert.ToInt32(reader["num"].ToString());
+                    studatalist.Add(tcsa);
+                }
+                reader.Close();
+                AccessDBHelper.CloseConnectDB();
+            });
+            //await Task.Delay(1000);
+            studatalist.Sort(new TeachClassStu());
+            this.namelist.ItemsSource = studatalist;
+            this.onestudent.DataContext = studatalist;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            selected = selected <= 0 ? studatalist.Count - 1 : selected - 1;
+            this.onestudent.DataContext = studatalist[selected];
+            speech.Speak(" ", SpeechVoiceSpeakFlags.SVSFlagsAsync);
+            speech.Speak(stuname.Text, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            selected = selected >= studatalist.Count - 1 ? 0 : selected + 1;
+            this.onestudent.DataContext = studatalist[selected];
+            speech.Speak(" ", SpeechVoiceSpeakFlags.SVSFlagsAsync);
+            speech.Speak(stuname.Text, SpeechVoiceSpeakFlags.SVSFlagsAsync);
+        }
     }
+
 }
