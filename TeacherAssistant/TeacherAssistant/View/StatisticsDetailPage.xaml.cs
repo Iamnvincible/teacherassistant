cae491f1f01661bcc8abd3c4b7a71d6798be6e8a
Lin Jie <ljlovemichael@hotmail.com>
2016-01-02 14:15:53 +0800

    统计作业Page开工

---
+++ TeacherAssistant/TeacherAssistant/View/StatisticsDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeacherAssistant.View
+{
+    /// <summary>
+    /// Interaction logic for StatisticsDetailPage.xaml
+    /// </summary>
+    public partial class StatisticsDetailPage : Page
+    {
+        public StatisticsDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Goback(object sender, RoutedEventArgs e)
+        {
+            var s = this.NavigationService.CanGoBack;
+            if (s)
+            {
+                this.NavigationService.GoBack();
+            }
+        }
+    }
+}
--- TeacherAssistant/TeacherAssistant/View/StatisticsPage.xaml.cs
+++ TeacherAssistant/TeacherAssistant/View/StatisticsPage.xaml.cs
@@ -42,5 +42,12 @@ namespace TeacherAssistant.View
         {
             spvm.Cd=new ObservableCollection<ClassDetail>(spvm.Cd.OrderBy(x=>x.CourseName).ToList());
         }
+
+        private void Statistics(object sender, RoutedEventArgs e)
+        {
+            Page p = new StatisticsDetailPage();
+
+            this.NavigationService.Navigate(p);
+        }
     }
 }
